17c7c1500db257bdc9a5b34ed85df01e3ef5fed2
beccaGee <beccag5253@gmail.com>
2026-08-31 16:29:17 -0700

    Form commit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,24 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    }, {
-      name: "Mac",
-      job: "Bouncer",
-    },{
-      name: "Dee",
-      job: "Aspiring",
-    },{
-      name: "Dennis",
-      job: "Bartender",
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -30,6 +17,7 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   );
 }
